d0ed9a0cd5684be5b368aee89ef427f9a8a22002
Babai <Babai@DESKTOP-SSNJ5BR>
2022-06-14 23:18:53 +0530

    hello stack

---
+++ Hemant/src/com/niit/hemant/HemantStack.java
@@ -0,0 +1,10 @@
+package com.niit.hemant;
+
+public class HemantStack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello Hemant admin!!");
+     }
+
+}
